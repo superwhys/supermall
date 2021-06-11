d1fb03806eb2d30f3d110e09033836b3644f2053
YongHWw <269085434@qq.com>
2021-06-11 22:06:24 +0800

    'tarbar+navbar'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+<!--    <img src="../../assets/img/tabbar/home.svg" alt="">-->
+<!--    <div>首页</div>-->
+    <slot v-if="!isActive" name="item-icon"></slot>
+    <slot v-else name="item-icon-activate"></slot>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data() {
+    return {
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      if(this.$route.matched[0].path != this.path) {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    /*均等分*/
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img{
+    margin-top: 4px;
+    /*去除img下方的空间*/
+    vertical-align: middle;
+    width: 24px;
+    height: 24px;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      // 配置别名
+      alias: {
+        'assets': '@/assets',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
+}
